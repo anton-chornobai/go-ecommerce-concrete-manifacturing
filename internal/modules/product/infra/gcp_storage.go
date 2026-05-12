76be507e1b9b335aa77e56d4d4aa3ea294b21e36
anton-chornobai <antonchornobajj1@gmail.com>
2026-05-12 10:02:20 +0300

    added addMultipleFiles method and also delete method

--- internal/modules/product/infra/gcp_storage.go
+++ internal/modules/product/infra/gcp_storage.go
@@ -2,8 +2,14 @@ package infra
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"mime/multipart"
+	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
 	"cloud.google.com/go/storage"
 )
@@ -34,4 +40,81 @@ func (s *GCPUploader) Upload(ctx context.Context, file io.Reader, filename strin
 	}
 
 	return fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, filename), nil
+}
+
+func (s *GCPUploader) UploadMultipleFiles(ctx context.Context, headers []*multipart.FileHeader) ([]string, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	type result struct {
+		index int
+		url   string
+		err   error
+	}
+
+	resultChan := make(chan result, len(headers))
+	var wg sync.WaitGroup
+
+	for i, header := range headers {
+		wg.Add(1)
+		go func(i int, h *multipart.FileHeader) {
+			defer wg.Done()
+
+			f, err := h.Open()
+			if err != nil {
+				resultChan <- result{index: i, err: err}
+				return
+			}
+			defer f.Close()
+
+			ext := filepath.Ext(h.Filename)
+			nameOnly := strings.TrimSuffix(h.Filename, ext)
+			uniqueName := fmt.Sprintf("%s_%d_%d%s", nameOnly, time.Now().UnixNano(), i, ext)
+
+			url, err := s.Upload(ctx, f, uniqueName)
+			if err != nil {
+				resultChan <- result{index: i, err: err}
+				return
+			}
+
+			resultChan <- result{index: i, url: url}
+		}(i, header)
+	}
+
+	go func() {
+		wg.Wait()
+		close(resultChan)
+	}()
+
+	urls := make([]string, len(headers))
+	for res := range resultChan {
+		if res.err != nil {
+			cancel()
+			return nil, fmt.Errorf("не вдалося завантажити файл: %w", res.err)
+		}
+		urls[res.index] = res.url
+	}
+
+	return urls, nil
+}
+
+func (s *GCPUploader) Delete(ctx context.Context, url string) error {
+	bucketName := "stezka_bucket"
+
+	prefix := fmt.Sprintf("https://storage.googleapis.com/%s/", bucketName)
+	filename := strings.TrimPrefix(url, prefix)
+	if filename == url {
+		return fmt.Errorf("невалідний url зображення: %s", url)
+	}
+
+	obj := s.client.Bucket(bucketName).Object(filename)
+	if err := obj.Delete(ctx); err != nil {
+		if errors.Is(err, storage.ErrObjectNotExist) {
+			// object already gone — not an error from our perspective
+			return nil
+		}
+		return fmt.Errorf("не вдалося видалити файл %s: %w", filename, err)
+	}
+
+	return nil
 }
